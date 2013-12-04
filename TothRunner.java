588f84201b3829b21a3bf91e12fa6278d759b6da
darkhorse20 <anuroopyadav@gmail.com>
2013-12-04 07:46:47 -0500

    Update TothRunner.java

--- TothRunner.java
+++ TothRunner.java
@@ -1,18 +1,29 @@
 package com.mfe.baruch.capstone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TothRunner {
 
 	/**
 	 * @param args
 	 */
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 	    long start = System.currentTimeMillis();
 		Parameters p = new Parameters();
 		
 		TothSimulator simulator = new TothSimulator(p);
-		simulator.runSimulations();
+		List<Book> books = simulator.runSimulations();
 		System.out.println("Time to run " + p.getNumSims() + ": " + (System.currentTimeMillis() - start));
 		
+		List<Integer> aveBookShape = new ArrayList<Integer>(2*p.getBand()+1);
+		
+		for(Book book:books) {
+			Utils.aggregateLists(aveBookShape, book.dynamicBookShape(p.getBand()));
+			
+		}
+		Utils.write(aveBookShape, "C:/aya/Documents/MFE/Baruch/Capstone/AveBookShape.csv");
 		System.out.println("Done");
 	}
 
