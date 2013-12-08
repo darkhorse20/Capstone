34b4958195f8ed41e6117998ab6ef1e68b91507d
darkhorse20 <anuroopyadav@gmail.com>
2013-12-08 10:59:35 -0500

    Create ParallelTothRunner.java

---
+++ ParallelTothRunner.java
@@ -0,0 +1,68 @@
+package com.mfe.baruch.capstone;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ParallelTothRunner {
+
+    /**
+     * @param args
+     */
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Parameters p = new Parameters();
+        List<Book> books = new ArrayList<Book>();
+        List<Double> aveMidPrxs = new ArrayList<Double>(p.getNumEvents());
+        ExecutorService executor =Executors.newFixedThreadPool(p.getThreads());
+        List<Future<RunState>> futureList = new ArrayList<Future<RunState>>(p.getNumEvents());
+        
+        for (int i=0; i < p.getNumSims(); i++) {
+            futureList.add(executor.submit(new CallableTothSimulator(p)));
+        }
+        
+        for(int i=0;i < futureList.size(); i++) {
+            try {
+                //System.out.println("Waiting for future : " + i);
+                RunState state = futureList.get(i).get();
+                books.add(state.getBook());
+                Utils.aggregateListsDouble(aveMidPrxs, state.getMidPrxs());
+                System.out.println("Waiting for future : " + i + "done");
+                
+            } catch (InterruptedException e) {
+                throw new RuntimeException( e );
+                
+            } catch (ExecutionException e) {
+                throw new RuntimeException( e );
+
+            }
+        }
+        System.out.println("Time to run " + p.getNumSims() + ": " + (System.currentTimeMillis() - start));
+        executor.shutdown();
+        
+        Utils.write(aveMidPrxs, p.getOutputFilePath() + "AveMidPrxs.csv");
+        
+        List<Integer> aveBookShape = new ArrayList<Integer>(2*p.getBand()+1);
+        List<List<Integer>> allbooks = new ArrayList<List<Integer>>(p.getNumSims());
+        
+        int i =0;
+        for(Book book:books) {
+            List<Integer> bookShape = book.dynamicBookShape(p.getBand());
+            Utils.aggregateLists(aveBookShape, bookShape);
+            allbooks.add(bookShape);
+            i++;
+        }
+        
+        Utils.write(aveBookShape, p.getOutputFilePath() + "AveBookShape.csv");
+        Utils.write(Utils.calcStdDev(allbooks), p.getOutputFilePath() + "BookShapes.csv");
+        
+        
+        
+        System.out.println("Done");
+    }
+
+}
