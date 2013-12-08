cc4b2e797d5849e483f2c1ae72831a8f4f5f55c8
darkhorse20 <anuroopyadav@gmail.com>
2013-12-08 11:04:54 -0500

    Update RunState.java

--- RunState.java
+++ RunState.java
@@ -8,31 +8,31 @@ import org.apache.commons.math3.distribution.PoissonDistribution;
 import org.apache.commons.math3.distribution.UniformRealDistribution;
 
 public class RunState {
-	private int currL;
-	private int currBS;
-	private Book book;
-	private PoissonDistribution mktPois = new PoissonDistribution(0.1);
-	private PoissonDistribution cxlPois = new PoissonDistribution(0.0001);
-	private PoissonDistribution limPois = new PoissonDistribution(0.5);
-	private UniformRealDistribution fracUnif;
-	private UniformRealDistribution bsUnif = new UniformRealDistribution();
-	private UniformRealDistribution mktbsUnif = new UniformRealDistribution();
-	private BinomialDistribution cxlBS;
-	private BinomialDistribution mktBS;
-	private Parameters runParams;
-	private List<Double> midPrxs;
-	
-	public RunState(Book book, Parameters params) {
-		this.currL = 0;
-		this.currBS = 2;
-		this.book = book;
-		this.runParams = params;
-		cxlBS = new BinomialDistribution(1, 0.5);
-		mktBS = new BinomialDistribution(1, 0.5);
-		fracUnif = new UniformRealDistribution(params.getZetaG(),1.0);
-		this.midPrxs  = new ArrayList<Double>(params.getNumEvents());
-		
-	}
+    private int currL;
+    private int currBS;
+    private Book book;
+    private PoissonDistribution mktPois = new PoissonDistribution(0.1);
+    private PoissonDistribution cxlPois = new PoissonDistribution(0.0001);
+    private PoissonDistribution limPois = new PoissonDistribution(0.5);
+    private UniformRealDistribution fracUnif;
+    private UniformRealDistribution bsUnif = new UniformRealDistribution();
+    private UniformRealDistribution mktbsUnif = new UniformRealDistribution();
+    private BinomialDistribution cxlBS;
+    private BinomialDistribution mktBS;
+    private Parameters runParams;
+    private List<Double> midPrxs;
+    
+    public RunState(Book book, Parameters params) {
+        this.currL = 0;
+        this.currBS = 2;
+        this.book = book;
+        this.runParams = params;
+        cxlBS = new BinomialDistribution(1, 0.5);
+        mktBS = new BinomialDistribution(1, 0.5);
+        fracUnif = new UniformRealDistribution(params.getZetaG(),1.0);
+        this.midPrxs  = new ArrayList<Double>(params.getNumEvents());
+        
+    }
 
 
     public int getCurrL() {
@@ -146,24 +146,24 @@ public class RunState {
     }
 
 
-	public List<Double> getMidPrxs() {
-		return midPrxs;
-	}
+    public List<Double> getMidPrxs() {
+        return midPrxs;
+    }
 
 
-	public void setMidPrxs(List<Double> midPrxs) {
-		this.midPrxs = midPrxs;
-	}
+    public void setMidPrxs(List<Double> midPrxs) {
+        this.midPrxs = midPrxs;
+    }
 
 
-	public UniformRealDistribution getMktbsUnif() {
-		return mktbsUnif;
-	}
+    public UniformRealDistribution getMktbsUnif() {
+        return mktbsUnif;
+    }
 
 
-	public void setMktbsUnif(UniformRealDistribution mktbsUnif) {
-		this.mktbsUnif = mktbsUnif;
-	}
-	
-	
+    public void setMktbsUnif(UniformRealDistribution mktbsUnif) {
+        this.mktbsUnif = mktbsUnif;
+    }
+    
+    
 }
